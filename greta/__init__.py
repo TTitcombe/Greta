f46174f2d36af7a7b296dc3b4b93cf3cb7678526
Tom Titcombe <t.j.titcombe@gmail.com>
2021-09-15 17:12:10 +0100

    Create func to condition variable on intensity

--- greta/__init__.py
+++ greta/__init__.py
@@ -1,3 +1,4 @@
 __version__ = "0.1.0"
 
 from .checks import check_intensity
+from .variables import condition_variable
---
+++ greta/variables.py
@@ -0,0 +1,38 @@
+"""
+Altering variables and values
+conditional on carbon intensity
+"""
+from .query import get_current_intensity
+from .utils import str_to_intensity_enum
+
+
+def condition_variable(low_value, high_value, limit: str):
+    """
+    Set a value conditional on carbon intensity
+
+    If carbon intensity is below or equal to a given threshold,
+    the `low_value` is given; otherwise `high_value` is given
+
+    Parameters
+    ----------
+    low_value : <T>
+        The value to return if carbon intensity is at or below
+        `limit`
+    high_value : <T>
+        The value to return if carbon intensity is above
+        `limit`
+    limit : str
+        The carbon intensity on which the values are conditioned
+
+    Returns
+    -------
+    <T>
+        `low_value` or `high_value`
+    """
+    limit_enum = str_to_intensity_enum(limit)
+    carbon_intensity_index = get_current_intensity()
+
+    if limit_enum.value <= carbon_intensity_index.value:
+        return low_value
+    else:
+        return high_value
