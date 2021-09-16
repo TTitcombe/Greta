5b2aeaa7f89f01e3683f9aa09efd346e9c6008d0
Tom Titcombe <t.j.titcombe@gmail.com>
2021-09-16 13:54:14 +0100

    Create function which skips code execution based on intensity
    Useful if unnecessary, intensive functionality in app
    
    re #12

--- greta/__init__.py
+++ greta/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.1.0"
 
-from .checks import intensity_check_error
+from .checks import intensity_check_error, intensity_check_skip
 from .conditionals import condition_function, condition_variable
--- greta/checks.py
+++ greta/checks.py
@@ -8,11 +8,11 @@ from .utils import str_to_intensity_enum
 
 def intensity_check_error(limit: str):
     """
-    Check carbon intensity before running code.
+    Error code execution if carbon intensity is beyond a threshold
 
     This func is to be wrapped around another function.
     If carbon intensity is not below the limit,
-    code will not run.
+    code will error.
 
     Parameters
     ----------
@@ -38,6 +38,37 @@ def intensity_check_error(limit: str):
                 )
 
             return func(*args, **kwargs)
+
+        return inner_func
+
+    return outer_func
+
+
+def intensity_check_skip(limit: str):
+    """
+    Skip code execution if carbon intensity is beyond a threshold
+
+    This func is to be wrapped around another function.
+    If carbon intensity is not below the limit,
+    the wrapped function will not run and "None" will be returned.
+
+    Parameters
+    ----------
+    limit : str
+        The maximum carbon intensity index under which the code should run
+        (inclusive).
+        Must be one of 'very low', 'low', 'moderate', 'high', 'very high'
+    """
+    limit_enum = str_to_intensity_enum(limit)
+
+    def outer_func(func):
+        def inner_func(*args, **kwargs):
+            carbon_intensity_index = get_current_intensity()
+
+            if carbon_intensity_index.value > limit_enum.value:
+                return
+            else:
+                return func(*args, **kwargs)
 
         return inner_func
 
